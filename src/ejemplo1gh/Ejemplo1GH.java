63bbf35462d0fb903db9e30fc7986f832075add7
Areli <Areli@LAPTOP-90IU5934>
2021-03-10 12:19:27 -0600

    Base del proyecto

---
+++ src/ejemplo1gh/Ejemplo1GH.java
@@ -0,0 +1,14 @@
+/* UVM  POO
+ * Areli Rosas 01-21
+ */
+package ejemplo1gh;
+
+import javax.swing.JOptionPane;
+
+public class Ejemplo1GH {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hola GitHub"); 
+    }
+    
+}
